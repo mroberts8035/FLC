c87af9adb63a34a1ba034ba519753dcc9ba1a2aa
Matthew Roberts <mroberts8035@outlook.com>
2022-03-29 20:47:45 -0500

    Worked on views and products

---
+++ StoreFront.UI.MVC/Controllers/EmployeesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StoreFront.Data.EF;
+
+namespace StoreFront.UI.MVC.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private StoreFrontEntities db = new StoreFrontEntities();
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            var employees = db.Employees.Include(e => e.Department).Include(e => e.Employee1);
+            return View(employees.ToList());
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "Description");
+            ViewBag.DirectReportID = new SelectList(db.Employees, "EmployeeID", "FName");
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmployeeID,FName,LName,DirectReportID,DepartmentID,HireDate,Address,City,State,Zip,Birthdate,Phone,Email")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "Description", employee.DepartmentID);
+            ViewBag.DirectReportID = new SelectList(db.Employees, "EmployeeID", "FName", employee.DirectReportID);
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "Description", employee.DepartmentID);
+            ViewBag.DirectReportID = new SelectList(db.Employees, "EmployeeID", "FName", employee.DirectReportID);
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmployeeID,FName,LName,DirectReportID,DepartmentID,HireDate,Address,City,State,Zip,Birthdate,Phone,Email")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "Description", employee.DepartmentID);
+            ViewBag.DirectReportID = new SelectList(db.Employees, "EmployeeID", "FName", employee.DirectReportID);
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
